9e6eb181911045ff74203f9604597ad69b74944f
Danil <28042001a@gmail.com>
2024-11-22 23:19:17 +0500

    Исправил замечания

--- src/app/index.ts
+++ src/app/index.ts
@@ -1,8 +1,1 @@
-// core app styles:
-import './styles/scss/_resert.scss'
-import './styles/scss/_global.scss'
-import './styles/scss/_variables.scss'
-import './styles/scss/_header.scss'
-// import './index.scss'
-
-// here you can init react or vue
+import './index.scss'
--- src/app/scripts/index.js
+++
@@ -1,1 +0,0 @@
-import '../styles/scss/_header.scss'
